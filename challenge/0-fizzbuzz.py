198894e621959506595203bb4242d1691f50ce53
moha88z <mohammed.alayda88@gmail.com>
2025-12-06 13:44:55 +0000

    0-fizzbuzz.py

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -5,13 +5,18 @@ import sys
 def fizzbuzz(n):
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
+            s = "FizzBuzz"
         elif i % 3 == 0:
-            print("Fizz", end=" ")
+            s = "Fizz"
         elif i % 5 == 0:
-            print("Buzz", end=" ")
+            s = "Buzz"
         else:
-            print(i, end=" ")
+            s = str(i)
+
+        if i != n:
+            print(s, end=" ")
+        else:
+            print(s, end="")
     print()
 
 
@@ -26,3 +31,4 @@ if __name__ == "__main__":
         max_n = 100
 
     fizzbuzz(max_n)
+
